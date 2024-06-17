65acbbb6144eaeab11caca4285b0efff28c02ce2
Matthew Wang <107924410+matthew-mw@users.noreply.github.com>
2024-06-17 21:10:09 +0000

    Fixed make_gif condition

--- python/main.py
+++ python/main.py
@@ -19,16 +19,16 @@ images = []
 grid = np.array([[not ((col % 2) and (row % 2)) for col in range(cols * 2 + 1)] for row in range(rows * 2 + 1)])
 
 for (cell, neighbor) in maze.make_maze():
+    cell_row = cell.row * 2 + 1
+    cell_col = cell.col * 2 + 1
+    neighbor_row = neighbor.row * 2 + 1
+    neighbor_col = neighbor.col * 2 + 1
+
+    grid[cell_row, cell_col] = 0
+    grid[neighbor_row, neighbor_col] = 0
+    grid[(cell_row + neighbor_row)//2, (cell_col + neighbor_col)//2] = 0
+
     if make_gif:
-        cell_row = cell.row * 2 + 1
-        cell_col = cell.col * 2 + 1
-        neighbor_row = neighbor.row * 2 + 1
-        neighbor_col = neighbor.col * 2 + 1
-
-        grid[cell_row, cell_col] = 0
-        grid[neighbor_row, neighbor_col] = 0
-        grid[(cell_row + neighbor_row)//2, (cell_col + neighbor_col)//2] = 0
-
         buf = io.BytesIO()
         plt.imsave(buf, grid, cmap=cmap, format="png")
         buf.seek(0)
